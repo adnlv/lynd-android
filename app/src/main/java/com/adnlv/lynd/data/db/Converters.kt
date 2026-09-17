d495900bc624f1bed559923c954863a00956df10
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-09-17 11:38:42 +0300

    Add Room type converters for LocalDate and BigDecimal

---
+++ app/src/main/java/com/adnlv/lynd/data/db/Converters.kt
@@ -0,0 +1,27 @@
+package com.adnlv.lynd.data.db
+
+import androidx.room.TypeConverter
+import java.math.BigDecimal
+import java.time.LocalDate
+
+class Converters {
+    @TypeConverter
+    fun fromLocalDate(date: LocalDate?): String? {
+        return date?.toString()
+    }
+
+    @TypeConverter
+    fun toLocalDate(value: String?): LocalDate? {
+        return value?.let { LocalDate.parse(it) }
+    }
+
+    @TypeConverter
+    fun fromBigDecimal(value: BigDecimal?): String? {
+        return value?.toPlainString()
+    }
+
+    @TypeConverter
+    fun toBigDecimal(value: String?): BigDecimal? {
+        return value?.let { BigDecimal(it) }
+    }
+}
