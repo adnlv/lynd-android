7a56236558d5bf654cfa30199b0c89314af04c7f
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-09-17 11:39:17 +0300

    Add BondDao to read and write bond data

---
+++ app/src/main/java/com/adnlv/lynd/data/db/BondDao.kt
@@ -0,0 +1,18 @@
+package com.adnlv.lynd.data.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface BondDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsertBond(bond: BondEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsertPayments(payments: List<BondPaymentEntity>)
+
+    @Query("SELECT * FROM bonds WHERE isin = :isin LIMIT 1")
+    suspend fun getBond(isin: String): BondEntity?
+}
