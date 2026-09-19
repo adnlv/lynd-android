d1396e32ccfe03605c5547b219bee893e6708f81
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-09-19 09:38:14 +0300

    Add light theme support using M3 baseline palette
    Support system dark and light modes by providing baseline Material 3 color schemes.

--- app/src/main/java/com/adnlv/lynd/ui/theme/Theme.kt
+++ app/src/main/java/com/adnlv/lynd/ui/theme/Theme.kt
@@ -1,17 +1,23 @@
 package com.adnlv.lynd.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 private val DarkColorScheme = darkColorScheme()
+private val LightColorScheme = lightColorScheme()
 
 @Composable
 fun LyndTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
     MaterialTheme(
-        colorScheme = DarkColorScheme,
+        colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
