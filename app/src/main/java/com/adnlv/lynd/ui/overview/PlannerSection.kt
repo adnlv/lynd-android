ea9ef8b807af8272b988eb00a5b5d9d8a4776807
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-09-25 22:12:56 +0300

    Add scrollable tab row to fit all planner tabs

--- app/src/main/java/com/adnlv/lynd/ui/overview/PlannerSection.kt
+++ app/src/main/java/com/adnlv/lynd/ui/overview/PlannerSection.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.SecondaryTabRow
+import androidx.compose.material3.SecondaryScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,8 +29,9 @@ fun PlannerSection(
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier.fillMaxSize()) {
-        SecondaryTabRow(
-            selectedTabIndex = uiState.selectedPlannerTab.ordinal
+        SecondaryScrollableTabRow(
+            selectedTabIndex = uiState.selectedPlannerTab.ordinal,
+            edgePadding = 16.dp
         ) {
             PlannerTab.entries.forEach { tab ->
                 Tab(
